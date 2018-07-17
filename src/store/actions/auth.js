36bd5ecab5d5808c238c3795b1f5c7339537dd1d
garfield <cmgarfield@gmail.com>
2018-07-17 23:12:53 +0800

    Lec.313, Storing the Token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
   };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (idToken, localId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData: authData
+    idToken: idToken,
+    userId: localId
   };
 };
 
@@ -37,7 +38,7 @@ export const auth = (email, password, isSignup) => {
     axios.post(url, authData)
       .then(response => {
         console.log(response);
-        dispatch(authSuccess(response.data))
+        dispatch(authSuccess(response.data.idToken, response.data.localId))
       })
       .catch(err => {
         console.log(err);
